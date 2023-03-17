ecdaf996d667975d1016a2ffd0098c525ddaf077
Kiko <kiko.cusatis@gmail.com>
2023-03-17 17:13:40 -0500

    Change completion to chat

--- conversation.py
+++ conversation.py
@@ -1,7 +1,7 @@
 import os
 import openai
-from characters import character
-from characters import voice
+import numpy
+import npc_dio_mod
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -12,12 +12,12 @@ character_name: str = "gerg"
 #============================================
 
 #Create character and populate with content
-gerg_char = character.Character(character_name, enviroment_name, world_directory) #This is all you'd be using in an application
+gerg_char = npc_dio_mod.Character(character_name, enviroment_name, world_directory) #This is all you'd be using in an application
 
 #Run conversation loop
 print("converstation simulator initializing...")
 
-context = ""
+convo = list()
 running = True
 while(running):
 
@@ -25,8 +25,11 @@ while(running):
 
     if(inp == "/stop"):
         running = False
+        continue
     
-    context += "\nPlayer: " + inp
-    response = gerg_char.name + ": " + gerg_char.talk(context)
-    context += "\n" + response
-    print(response)
+    
+    convo.append({"role" : "user", "content" : inp})
+    response = gerg_char.talk(convo)
+
+    convo.append({"role" : "assistant", "content" : response})
+    print(gerg_char.name + ": " + response)
