2f6ee27b25b91522e681abd8268f4542884b369c
Kiko <kiko.cusatis@gmail.com>
2023-04-15 14:14:57 -0500

    Added actions

--- conversation.py
+++ conversation.py
@@ -5,18 +5,33 @@ import npc_dio_mod
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #===================Params===================
-world_directory: str = "./_silk_road"
-enviroment_name: str = "Ervok"
-character_name: str = "Satik_the_Cheap"
+world_directory: str = "./_solarpunk"
+enviroment_name: str = "herville"
+character_name: str = "dedrick"
 #============================================
 
 #Create character and populate with content
 gerg_char = npc_dio_mod.Character(character_name, enviroment_name, world_directory) 
 #dedrick_char = npc_dio_mod.Character("Dedrick", enviroment_name, world_directory) 
 
+
 #Run conversation loop
 print("converstation simulator initializing...")
 running = True
+
+def leave():
+    print("conversation ended by " + character_name)
+    running = False
+
+#Actions
+leave_convo = ("Leave", "Leave the conversation immediately", leave)
+show_item = ("Show_Item", "Show an item to the player. This gives them the item temporarily.", lambda: print("[Showing item]"))
+
+actions = list()
+actions.append(leave_convo)
+actions.append(show_item)
+
+gerg_char = npc_dio_mod.Character(character_name, enviroment_name, world_directory, actions = actions) 
 
 while(running):
 
--- npc_dio_mod/character.py
+++ npc_dio_mod/character.py
@@ -15,13 +15,16 @@ class Character:
     personal: str
     considerations: str
 
+    actions: list()
+
     thoughts: str
     mind: ai.AIChat
 
     conversation: list
+    memory: str
     debug_log: str
 
-    def __init__(self, character_name: str, enviroment_name: str, world_directory: str):
+    def __init__(self, character_name: str, enviroment_name: str, world_directory: str, actions = ""):
         self.name = character_name.capitalize()
         
         self.world = open(f"{world_directory}/world.prompt", "r").read()
@@ -37,18 +40,23 @@ class Character:
         else:
             self.considerations = open(f"{world_directory}/considerations.prompt", "r").read()
 
+        if (actions == ""):
+            self.actions = list()
+        else:
+            self.actions = actions
+
         self.mind = ai.AIChat(model="gpt-3.5-turbo")
         self.thoughts = ""
         self.conversation = list()
 
         self.debug_log = ""
 
+#Knowledge Compilation
     def self_knowledge(self):
         return f"{self.physical_desc}\n\n{self.personal_desc}"
 
     def world_knowledge(self):
         return f"{self.world}\n\n{self.enviroment}"
-
 
     def format_knowledge(self):
         knowledge = (f"* Knowledge:\n"
@@ -57,6 +65,51 @@ class Character:
                     f"{self.self_knowledge()}\n"
                     )
         return knowledge
+
+    def format_actions(self):
+        action_list = ""
+        
+        for a in self.actions:
+            action_list += (
+                f" - {a[0]}\n"
+                f"{a[1]}\n"
+                f"\n"
+            )
+
+        return action_list
+
+
+    # summarize memory into a knowledge
+    # def sleep(self):
+    
+#Prompting
+    
+    def flatten_convo(self):
+        convo = ''
+        for i in self.conversation:
+
+            role = "OTHER"
+            if(i['role']=="assistant"):
+                role = self.name.upper()
+
+            convo += f"{role}: {i['content']}"
+        
+        return convo.strip()
+
+    def thoughts_prompt(self):
+
+        prompt = ai.Prompt("npc_dio_mod/provoke_thoughts")
+        prompt.write("ABOVE", self.format_knowledge())
+        prompt.write("CHAT", self.flatten_convo())
+        prompt.write("NAME", self.name)
+        t = self.thoughts
+        if(t == ""):
+            t = "no previous thoughts."
+        prompt.write("PREV", t)
+        prompt.write("ACTIONS", self.format_actions())
+        prompt.write("CONSIDERATIONS", self.considerations)
+
+        return prompt.read()
 
     def response_prompt(self):  
 
@@ -70,53 +123,71 @@ class Character:
         
         return prompt
 
-    def thoughts_prompt(self):
+    def action_prompt(self, response):
 
-        prompt = ai.Prompt("npc_dio_mod/provoke_thoughts")
-        prompt.write("ABOVE", self.format_knowledge())
+        prompt = ai.Prompt("npc_dio_mod/evaluate_actions")
+        # prompt.write("ABOVE", self.self_knowledge())
+
+        prompt.write("ACTIONS", self.format_actions())
         prompt.write("CHAT", self.flatten_convo())
         prompt.write("NAME", self.name)
-        prompt.write("PREV", self.thoughts)
-        prompt.write("CONSIDERATIONS", self.considerations)
+        prompt.write("THOUGHTS", self.thoughts)
+        prompt.write("RESPONSE", response)
 
         return prompt.read()
 
-    def flatten_convo(self):
-        convo = ''
-        for i in self.conversation:
+    def actions_to_functions(self,response):
+        response = response.split()
 
-            role = "OTHER"
-            if(i['role']=="assistant"):
-                role = self.name.upper()
+        taken_actions = filter(lambda a: (a[0] in response), self.actions)
+        output = list()
+        for a in taken_actions:
+            output.append(a[2])
 
-            convo += f"{role}: {i['content']}"
-        
-        return convo.strip()
+        return output
+    # prompt character to choose (from a list of feelings and emotions) how it feels in the given situation
+    # def query(self, situation):
 
+#Actions
     def talk(self, input, debug: bool = False):
 
         self.conversation.append({"role" : "user", "content" : input})
 
+        #Prompt for forethought
         prompt = self.thoughts_prompt()
         self.thoughts = self.mind.evaluate(prompt)
 
+        #Prompt for verbal response
         prompt = self.response_prompt()
         response = self.mind.evaluate(prompt)
+
+        #Prompt for action
+        prompt = self.action_prompt(response)
+        actions = self.mind.evaluate(prompt)
+        actions_do = self.actions_to_functions(actions)
+        for a in actions_do:
+            a()
 
         self.conversation.append({"role" : "assistant", "content" : response})
         # response = voice.Voice.revise_response(response, self.dialect_table)
 
         self.debug_log = (
                 f"\n========================== DEBUG ====================================\n"
+                f"=== STIMULI ===\n"
+                f"{input}\n\n"
                 f"=== INTERNAL ===\n"
                 f"{self.thoughts}\n\n"
                 f"=== RESPONSE ===\n"
                 f"{response}\n"
+                f"=== ACTIONS ===\n"
+                f"{self.format_actions()}"
+                f"{actions}\n"
                 f"========================== END DEBUG =================================\n"
         )
         if(debug):
             print(self.debug_log)
-        return response
+    
 
-    # prompt character to choose (from a list of feelings and emotions) how it feels in the given situation
-    # def query(self, situation):
+        return response
+    
+    # def leave(self)
